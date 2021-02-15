4c7e2b461c6785d771b06f816a8f6149c486bc5e
ElijahKamski <yarikkhripkov@gmail.com>
2021-02-15 12:33:09 +0300

    added comments

--- lab1/Palindrome.java
+++ lab1/Palindrome.java
@@ -1,10 +1,20 @@
 public class Palindrome {
+    /** Считываем строки из аргументов и передаем их в функцию, после чего выводим её результат
+     *
+     * @param args - строки на вход
+     */
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String s = args[i];
             System.out.println(isPalindrome(s));
         }
     }
+
+    /**
+     *
+     * @param a - строка, которую надо развернуть
+     * @return развернутая строка
+     */
     public static String reverseString(String a){
         String s=a;
         String sn="";
@@ -13,6 +23,12 @@ public class Palindrome {
         }
         return sn;
     }
+
+    /**
+     *
+     * @param s - Возможный палиндром
+     * @return - является ли строка палиндромом
+     */
     public static boolean isPalindrome(String s){
         return s.equals(reverseString(s));
     }
--- lab1/Primes.java
+++ lab1/Primes.java
@@ -1,11 +1,21 @@
 public class Primes {
-    public static void main(String[] args) {
+    /**
+     *
+     * пройтись по всем числам от 2 до 100 и проверить их на "простоту"
+     */
+    public static void main() {
         for (int i=2;i<=100;i++){
             if (isPrime(i)){
                 System.out.print(i+" ");
             }
         }
     }
+
+    /**
+     *
+     * @param n -возможно простое число
+     * @return вернуть является ли число простым
+     */
     public static boolean isPrime(int n){
         for (int i=2; i*i<n; i++){
             if (n%i==0){
